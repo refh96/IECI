cf0a410bb1dbce9a6572a0a932d4836d5ad77dca
CristopherQuintana <crisquintana33@gmail.com>
2022-11-14 03:23:23 -0300

    Limpieza

--- routes/categoryRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const categoryController = require('../controllers/categoryController');
-const api = express.Router();
-
-api.post('/category/:id', categoryController.createCategory);
-
-module.exports = api
