a8e0b6d4df80e03274c528305713a1160668bede
CristopherQuintana <crisquintana33@gmail.com>
2023-01-11 18:01:55 -0300

    quitar bootstrap

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,3 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import '../styles/index.global.css'
 import { ChakraProvider } from '@chakra-ui/react';
 export default function App({ Component, pageProps }) {
   return (
