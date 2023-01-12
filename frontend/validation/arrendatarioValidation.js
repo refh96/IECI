aaa5fdcb473a3fb7c1ebce0ccb649d9f6af2eca3
rodrigo flores hernandez <rodrigo.flores1501@alumnos.ubiobio.cl>
2023-01-11 23:39:46 -0300

    arreglos

--- frontend/validation/arrendatarioValidation.js
+++ frontend/validation/arrendatarioValidation.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import * as yup from 'yup'
 
 const arrendatarioValidation = yup.object({
@@ -20,27 +20,4 @@ const arrendatarioValidation = yup.object({
     .matches(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,"El correo debe seguir el formato de correo")
 })
 
-=======
-import * as yup from 'yup'
-
-const arrendatarioValidation = yup.object({
-    nombre: yup.string().min(3, "Debe contener mínimo 3 caracteres")
-    .max(50, "Debe contener máximo 50 caracteres")
-    .required("El nombre es obligatorio")
-    .matches(/^[a-zA-Z0-9 ]$/,"El nombre no puede contener caracteres especiales"),
-    apellido: yup.string().min(3, "Debe contener mínimo 3 caracteres")
-    .max(50, "Debe contener máximo 50 caracteres")
-    .required("El nombre es obligatorio")
-    .matches(/^[a-zA-Z0-9 ]$/,"El nombre no puede contener caracteres especiales"),
-    número: yup.number().min(8)
-    .max(8, "Debe contener 8 caracteres")
-    .required("El número es obligatorio")
-    .matches(),
-    correo: yup.string().min(7, "Debe contener mínimo 7 caracteres")
-    .max(50, "Debe contener máximo 50 caracteres")
-    .required("El correo es obligatorio")
-    .matches(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,"El correo debe seguir el formato de correo")
-})
-
->>>>>>> 1046fdaa2324401147a7f39448425ab6a9c6e78a
 export default arrendatarioValidation
